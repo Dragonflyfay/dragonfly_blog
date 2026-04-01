cfed7a5fa12bfea3d99b69ca0564b42a6b0258f7
Dragonflyfay <1504195035@qq.com>
2026-04-01 20:49:51 +0800

    feat:用户注册接口

---
+++ src/main/java/com/dragonfly/exception/GlobalExceptionHandle.java
@@ -0,0 +1,23 @@
+package com.dragonfly.exception;
+
+import com.dragonfly.pojo.Result;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 描述：
+ *
+ * @param
+ * @author 蜻蜓大王
+ * @date 2026/3/31 14:43
+ */
+@RestControllerAdvice
+public class GlobalExceptionHandle {
+    @ExceptionHandler(Exception.class)
+    public Result handleException(Exception e){
+        e.printStackTrace();
+        return Result.error(StringUtils.hasLength(e.getMessage())?e.getMessage():"操作失败");
+
+    }
+}
