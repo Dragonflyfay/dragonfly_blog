daec21436fbec6a9cdef238d891344922359395a
Dragonflyfay <1504195035@qq.com>
2026-04-08 23:17:53 +0800

    fix:用ThreadLocal优化

--- src/main/java/com/dragonfly/interceptors/LoginInterceptor.java
+++ src/main/java/com/dragonfly/interceptors/LoginInterceptor.java
@@ -2,8 +2,10 @@ package com.dragonfly.interceptors;
 
 import com.dragonfly.pojo.Result;
 import com.dragonfly.utils.JwtUtil;
+import com.dragonfly.utils.ThreadLocalUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -22,9 +24,12 @@ public class LoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         //令牌验证
         String token = request.getHeader("Authorization");
+
         //验证token
         try {
             Map<String,Object> claims= JwtUtil.parseToken(token);
+            //把业务数据存储到ThreadLocal中，供后续业务使用
+            ThreadLocalUtil.set(claims);
             //放行
             return true;
         } catch (Exception e) {
@@ -33,5 +38,11 @@ public class LoginInterceptor implements HandlerInterceptor {
             //不放行
             return false;
         }
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+       //清理ThreadLocal
+        ThreadLocalUtil.remove();
     }
 }
