28dfdc2ee0b35c369e97f94d70b44f65649e2667
Dragonflyfay <202401020345@stu.hnucm.edu.cn>
2026-04-21 20:17:54 +0800

    feat:获取文章分类列表

--- src/main/java/com/dragonfly/pojo/Category.java
+++ src/main/java/com/dragonfly/pojo/Category.java
@@ -1,5 +1,6 @@
 package com.dragonfly.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
@@ -17,6 +18,8 @@ public class Category {
     private Integer sortOrder;
     private Integer articlesCount;
     private Integer createUser;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime updateTime;
 }
